43d8e2f40430aa5769fb02c6770170d3e6ade7bf
BacHK <hokhacbac@gmail.com>
2017-05-23 19:53:03 +0700

    Times: 2 Day: 23/5/2017

---
+++ godtowerbase-master/Assets/Scripts/GateKeeper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GateKeeper : MonoBehaviour {
+
+    public string levelNumber;
+    public string password;
+    public string nextSceneName;
+
+    public Text levelText;
+    public InputField passwordInput;
+    public Text accessDeniedText;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad % 2 < 1)
+        {
+            levelText.text = "LEVELS";
+        }
+        else
+        {
+            levelText.text = levelNumber;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            nextButtonOnclick();
+        }
+
+    }
+
+    public void nextButtonOnclick()
+    {
+        if(string.IsNullOrEmpty(passwordInput.text.Trim()))
+        {
+            return;
+        }
+        if(password == passwordInput.text)
+        {
+            TKSceneManager.ChangeScene("Level 2");
+        }
+        else
+        {
+            passwordInput.text = "";
+            accessDeniedText.gameObject.SetActive(true);
+        }
+    }
+}
