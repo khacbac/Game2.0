b51521a82fa7e192863332bd23517f80fb796f36
BacHK <hokhacbac@gmail.com>
2017-05-28 06:57:00 +0700

    Content: Complete Day: 28/5/2017

---
+++ Game2.0-master/godtowerbase-master/Assets/Scripts/GateKeeper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GateKeeper : MonoBehaviour
+{
+
+    public string levelNumber;
+    public string password;
+    public string nextSceneName;
+    private bool check = true;
+
+    public Text levelText;
+    public InputField passwordInput;
+    public Text accessDeniedText;
+    public Button btnIconNext;
+
+    public List<GameObject> hints;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad % 2 < 1)
+        {
+            levelText.text = "LEVELS";
+        }
+        else
+        {
+            levelText.text = levelNumber;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            submitButtonOnclick();
+        }
+
+    }
+
+    public void submitButtonOnclick()
+    {
+        if (string.IsNullOrEmpty(passwordInput.text.Trim()))
+        {
+            return;
+        }
+        if (password == passwordInput.text)
+        {
+            TKSceneManager.ChangeScene(nextSceneName);
+        }
+        else
+        {
+            passwordInput.text = "";
+            accessDeniedText.gameObject.SetActive(true);
+        }
+    }
+
+    public void changeToSuggestion()
+    {
+        if (check)
+        {
+            hints[1].gameObject.SetActive(true);
+            hints[0].gameObject.SetActive(false);
+            btnIconNext.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            hints[1].gameObject.SetActive(false);
+            hints[0].gameObject.SetActive(true);
+            btnIconNext.gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        check = !check;
+
+    }
+
+}
---
+++ Game2.0-master/godtowerbase-master/Assets/Scripts/Level5ImgController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Level5ImgController : EventTrigger {
+
+    private Vector2 mouseOffset;
+
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = new Vector3(
+            Random.Range(-277.5f, 277.5f),
+            Random.Range(-224, 256),
+            0
+            );
+
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        Vector3 worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 canvasMouse = transform.parent.InverseTransformPoint(worldMouse);
+
+        mouseOffset = canvasMouse - transform.localPosition;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        Vector3 worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 canvasMouse = transform.parent.InverseTransformPoint(worldMouse);
+        Vector2 expectPosition = (Vector2)canvasMouse - mouseOffset;
+
+        transform.localPosition = new Vector3(
+            Mathf.Clamp(expectPosition.x, -277.5f, 277.5f),
+            Mathf.Clamp(expectPosition.y, -224, 256),
+            0
+            );
+
+    }
+}
