4aa872b65183a953d896f478375d46238d8921bb
BacHK <hokhacbac@gmail.com>
2017-06-05 22:16:55 +0700

    Content: Physic Day 1 Day: 5/6/2017

---
+++ Game2.0-master/SkippyMonkey-Start-master/Assets/Scripts/AbstacleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstacleFactory : MonoBehaviour {
+
+    public GameObject obstaclePrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject newObsacle = Instantiate(
+            obstaclePrefab,
+            new Vector3(-200, 200, 0), 
+            Quaternion.identity); // ko xoay.
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
