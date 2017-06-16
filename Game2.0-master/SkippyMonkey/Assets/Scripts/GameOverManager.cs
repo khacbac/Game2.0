3e6ccc86cd7708314f2d155aa7e74f4dd1efcdb4
BacHK <hokhacbac@gmail.com>
2017-06-16 23:47:48 +0700

    Content: Complete Day: 16/6/2017

--- Game2.0-master/SkippyMonkey/Assets/Scripts/GameOverManager.cs
+++ Game2.0-master/SkippyMonkey/Assets/Scripts/GameOverManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameOverManager : MonoBehaviour {
+public class GameOverManager : MonoBehaviour
+{
 
     public Text scoreText;
 
     private int score;
 
+    private int listScoreCount;
+
     private void Start()
     {
-        score = PlayerPrefs.GetInt("SCORE");
+        listScoreCount = PlayerPrefs.GetInt("LIST_SCORE_COUNT");
+        for (int i = 0; i < listScoreCount; i++)
+        {
+            score = PlayerPrefs.GetInt("SCORE" + i);
+        }
         scoreText.text = "" + score;
     }
 
